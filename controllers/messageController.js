1cd868cbcef01763aa582dd9999df8c6dfe39850
Aswin110 <aswinashok110@gmail.com>
2024-01-04 21:10:49 +0530

    added message details page

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,20 @@
 const asyncHandler = require('express-async-handler');
-const message = require('../models/message');
-const user = require('../models/user');
+const Message = require('../models/message');
+const User = require('../models/user');
 
 exports.messageDetail = asyncHandler(async(req, res, next)=> {
-	res.send('message detailed page not implemented');
+	const message = await Message.findById(req.params.id).populate('user').exec();
+
+	if(!message) {
+		const err = new Error('Message not found');
+		err.status = 404;
+		next(err);
+	}
+
+	res.render('message-post', {
+		title : message.title,
+		message:message,
+	});
 });
 
 exports.message_new_get = asyncHandler(async(req, res, next)=> {
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
 	content: ["./views/**/*.pug"],
 	theme: {
 		extend: {},
+		fontSize: {
+			'9xl': '5rem', 
+		},
+		screens: {
+			sm: "640px",
+			md: "728px",
+			lg: "903px",
+			xlg: "1023px",
+		},
 	},
 	plugins: [],
 };
